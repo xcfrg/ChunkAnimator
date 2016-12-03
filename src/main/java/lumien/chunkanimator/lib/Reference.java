18785c57dbbf00b91570d8f2ec23e382c908550a
Lumien <lumien231@users.noreply.github.com>
2016-12-03 13:43:28 +0100

    lower case mod id

--- src/main/java/lumien/chunkanimator/lib/Reference.java
+++ src/main/java/lumien/chunkanimator/lib/Reference.java
@@ -2,7 +2,7 @@ package lumien.chunkanimator.lib;
 
 public class Reference
 {
-	public static final String MOD_ID = "ChunkAnimator";
+	public static final String MOD_ID = "chunkanimator";
 	public static final String MOD_NAME = "Chunk Animator";
 	public static final String MOD_VERSION = "@VERSION@";
 }
